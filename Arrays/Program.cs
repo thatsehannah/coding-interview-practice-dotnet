a40bb0a793eab3b9b951dfb9bf73644ef10933b0
E. Hannah <echannah631@gmail.com>
2023-08-24 15:40:42 -0500

    remove even integers from arraysolution

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Arrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RemoveEvenIntegersSolution();
+        }
 
+        static void RemoveEvenIntegersSolution()
+        {
+            int[] arr = { 1, 2, 4, 5, 10, 6, 3 };
+            arr = RemoveEvenIntegers.Solution(arr);
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
---
+++ Arrays/RemoveEvenIntegers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Arrays
+{
+	public class RemoveEvenIntegers
+	{
+        public static int[] Solution(int[] arr)
+        {
+            //This variable will serve two purposes:
+            //1. will be used so I can put the odd numbers at the front of the array
+            //2. will also serve as the size of the new array that I will be returning which will contain all of the odd numbers from 0 up to indexForOddNum
+            int indexForOddNum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    arr[indexForOddNum] = arr[i]; //moving the odd numbers to the front of the arr array for easier access later
+                    indexForOddNum++; //increasing the index
+                }
+            }
+
+            int[] tempArray = new int[indexForOddNum]; //creating a new array with a fixed size equal to indexForOddNum since this also indicates how many odd numbers we have in the array
+
+            for (int i = 0; i < indexForOddNum; i++)
+            {
+                tempArray[i] = arr[i];
+            }
+
+            return tempArray;
+        }
+    }
+}
+
