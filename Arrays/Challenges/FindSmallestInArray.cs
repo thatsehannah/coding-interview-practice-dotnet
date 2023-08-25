69ca58b0ae4d42db5c95a980f619439d50ab8470
E. Hannah <echannah631@gmail.com>
2023-08-25 08:04:00 -0500

    comments

--- Arrays/Challenges/FindSmallestInArray.cs
+++ Arrays/Challenges/FindSmallestInArray.cs
@@ -7,15 +7,15 @@ namespace Arrays
         //Implement a function thats takes an array arr and returns the smallest number in the given array.
         public static int Solution(int[] arr)
         {
-            //Array.Sort(arr);
+            //Array.Sort(arr); ----> my original solution
             //return arr[0];
 
-            int smallest = arr[0];
+            int smallest = arr[0]; //set smallest initially to the first element of the array
             for (int i = 1; i < arr.Length; i++)
             {
-                if (arr[i] < smallest)
+                if (arr[i] < smallest) //check if the element at the current index i is smaller than the current value of smallest
                 {
-                    smallest = arr[i];
+                    smallest = arr[i]; //set smallest equal to arr[i]
                 }
             }
 
